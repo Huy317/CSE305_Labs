227fd7e7a1503339d00d7eccd88aaf8d455903b2
Huy317 <167590855+Huy317@users.noreply.github.com>
2025-04-26 08:59:25 +0700

    added property class

--- Rental_Management/src/Property.java
+++ Rental_Management/src/Property.java
@@ -3,6 +3,11 @@ public class Property {
     String description;
     String ownerId;
 
+    public Property(String propertyId, String description, String ownerId) {
+        this.propertyId = propertyId;
+        this.description = description;
+        this.ownerId = ownerId;
+    }
     
     
 }

8f7539c7bb1931bcf3ec3e2c9a8b9ae3f9d2c2d1
Huy317 <167590855+Huy317@users.noreply.github.com>
2025-05-24 10:45:41 +0700

    update app

--- Lab5/src/App.java
+++ Lab5/src/App.java
@@ -1,9 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        ContractBuilder contractBuilder = new PermanentContractBuilder().buildContractID("1").buildRentAmount(999);
-        System.out.println(contractBuilder.signContract().toString());
+        ContractBuilder contractBuilder = new PermanentContractBuilder().buildContractID("1").buildPropertyID("p-1").buildTenantID("t-1").buildRentAmount(999);
+        Contract contract = contractBuilder.signContract();
+        System.out.println(contract.toString());
 
-        String encryptd = AES.encrypt("HelloWorld123", "password");
+        String encryptd = AES.encrypt(contract.toString(), "password");
         System.out.println(encryptd);
         String decrypted = AES.decrypt(encryptd, "password");
         System.out.println(decrypted);
